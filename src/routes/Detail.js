aec2336bc51de90d28880308f60cdd179b5c28ef
luna <lunabright817@gmail.com>
2021-11-02 09:00:19 +0900

    vanilla redux(nomad coder)

---
+++ src/routes/Detail.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function Detail({ toDo }) {
+
+  return(
+    <h1>{toDo?.text}</h1>
+  )
+};
+
+function mapStateToProps(state, ownProps){
+  const { match: { params: { id }}} = ownProps;
+  console.log(id)
+  return { 
+    toDo: state.find(
+      toDo => toDo.id === parseInt(id)
+    ) 
+  };
+}
+
+export default connect(mapStateToProps) (Detail);
